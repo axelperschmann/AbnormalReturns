805ea1b2a0f41c3b6f4c4a2c98a966e0ecfd5270
sfeuerriegel <stefan.feuerriegel@is.uni-freiburg.de>
2016-07-24 19:50:29 +0200

    Refactoring * Changed method signature * Extract plotEventStudy

--- R/abnormalReturn.R
+++ R/abnormalReturn.R
@@ -1,6 +1,6 @@
-#' computeAbnormalReturns function.
+#' Calculation of abnormal returns
 #'
-#' @description \code{computeAbnormalReturn} implements the event study
+#' This functions implements the event study
 #'   methodology and abnormal returns in particular. The event study methodology
 #'   is a common way to study the effects of certain events on stock prices. It
 #'   thus calculates a so-called abnormal return that measures the impact
@@ -9,25 +9,25 @@
 #'   Afterwards, one calculates the difference between the actual return and the
 #'   previously predicted normal return, i.e. the abnormal return.
 #'
-#' @param portfolio,commodity Data frames, minimally containing a column
+#' @param prices_market,prices_stock Data frames, minimally containing a column
 #'   \code{Date} and a second column named as specified by
-#'   \code{attributeOfInterest}. Time series data of portfolio and commodity
+#'   \code{attributeOfInterest}. Time series data of prices_market and prices_stock
 #'   performance respectively.
-#' @param regressionType A character object. Possible types are:
+#' @param regression A character object. Possible types are:
 #'   \itemize{
-#'      \item \strong{\code{'OLS'}} for Ordinary Least Squares.
+#'      \item \strong{\code{"OLS"}} for Ordinary Least Squares.
 #'    }
 #'    All other types give an error.
 #' @param eventIndex Positive integer, sequence of integers, or \code{NULL}.
 #' If \code{eventIndex} is a positive integer or sequence of integers, function computes
 #' abnormalReturns for the defined indices only. If \code{eventIndex=NULL}, function
 #' computes abnormalReturns for all data points in the range of
-#' \code{eventWindowLength+1:nrow{portfolio}}.
+#' \code{eventWindowLength+1:nrow{prices_market}}.
 #' @param estimationWindowLength Positive integer. Defines number of
 #'   observations considered to estimate model(s).
 #' @param attributeOfInterest A character object. Defines the attribute of
 #'   interest.
-#' @param showPlot A boolean value. Should a plot of the commodity performance
+#' @param showPlot A boolean value. Should a plot of the prices_stock performance
 #'   be shown?
 #' @return \code{computeAbnormalReturn} returns a data frame, comprising following columns:
 #'   \itemize{
@@ -36,238 +36,208 @@
 #'      \item \strong{\code{R.squared}} Numerical, between 0 and 1. The higher the
 #'      \code{R.squared}, the greater is the variance reduction of the abnormal return
 #'      - i.e. detecting the effect better.
-#'      \item \strong{\code{commodityReturn}} Numerical. Actual return of commodity.
-#'      \item \strong{\code{portfolioReturn}} Numerical. Actual return of portfolio.
+#'      \item \strong{\code{prices_stockReturn}} Numerical. Actual return of prices_stock.
+#'      \item \strong{\code{prices_marketReturn}} Numerical. Actual return of prices_market.
 #'    }
-#'    The number of rows returned depends on the length of \code{commodity}/\code{portfolio},
+#'    The number of rows returned depends on the length of \code{prices_stock}/\code{prices_market},
 #'    as well as \code{estimationWindowLength} and \code{eventIndex}.
-#' @keywords abnormalReturn portfolio commodity stock performance financial
-#'   markets values event impact
-#' @export
 #' @examples
-#' x <- computeAbnormalReturn(portfolio=d.DAX, commodity=d.VW, regressionType='OLS',
-#'                            estimationWindowLength=20, attributeOfInterest='Close',
+#' x <- computeAbnormalReturn(prices_market=d.DAX, prices_stock=d.VW, regression="OLS",
+#'                            estimationWindowLength=20, attributeOfInterest="Close",
 #'                            showPlot=TRUE)
 #' head(x)
 #' summary(x$R.squared)
 #' summary(x$abnormalReturn)
-#' @importFrom utils read.csv
-#' @importFrom graphics plot legend points
+#' @keywords abnormalReturn prices_market prices_stock stock performance financial
+#'   markets values event impact
 #' @importFrom stats lm predict
-#' @importFrom utils data
-computeAbnormalReturn <-
-  function(portfolio,
-           commodity,
-           regressionType = 'OLS',
-           eventIndex = NULL,
-           estimationWindowLength = 20,
-           attributeOfInterest = 'Close',
-           showPlot = FALSE) {
-    ## start by validating given parameters
-    if (estimationWindowLength < 3) {
-      # a minimum of two points is necessary for a linear regression.
-      stop(
-        "Error! A minimum estimation window size of 3 is necessary for this linear regression to produce meaningful results."
-      )
-    }
-
-    # check wether dataset contains attribute Date and attributeOfInterest
-    if (!'Date' %in% names(portfolio)) {
-      stop(
-        paste(
-          "Error! Portfolio data does not contain attribute 'Date'. Available Attributes: ",
-          paste(names(data), collapse = ", "),
-          sep = ''
-        )
-      )
-    }
-    if (!'Date' %in% names(commodity)) {
-      stop(
-        paste(
-          "Error! Commodity data does not contain attribute 'Date'. Available Attributes: ",
-          paste(names(data), collapse = ", "),
-          sep = ''
-        )
-      )
-    }
-    if (!attributeOfInterest %in% names(portfolio)) {
-      stop(
-        paste(
-          "Error! Portfolio data does not contain attribute '",
-          attributeOfInterest,
-          "'. Available Attributes: ",
-          paste(names(data), collapse = ", "),
-          sep = ''
-        )
-      )
-    }
-    if (!attributeOfInterest %in% names(commodity)) {
-      stop(
-        paste(
-          "Error! Commodity data does not contain attribute '",
-          attributeOfInterest,
-          "'. Available Attributes: ",
-          paste(names(data), collapse = ", "),
-          sep = ''
-        )
-      )
-    }
-
-    # check wether portfolio and commodity data sets fit together
-    if (nrow(portfolio) != nrow(commodity)) {
-      stop(
-        paste(
-          "Error! Portfolio and commodity data should be of same length:",
-          nrow(portfolio),
-          "vs.",
-          nrow(commodity)
-        )
-      )
-    }
-    for (check in (portfolio$Date == commodity$Date)) {
-      if (check == FALSE) {
-        stop("Error! Dates of portfolio and commodity data sets do not match.")
-      }
-    }
-
-    # check for which indices the abnormalReturns shall be calculated
-    if (is.null(eventIndex)) {
-      indices = (estimationWindowLength + 1):nrow(portfolio)
-    } else {
-      if (eventIndex > (estimationWindowLength)) {
-        indices = eventIndex
-      }
-      else {
-        stop(
-          "Error! Chosen eventIndex overlaps with estimationWindow. Choose higher eventIndex!"
-        )
-      }
-    }
-
-    collect.abnRet = c()
-    for (idx in indices) {
-      # indices of data points used for estimating the OLS model
-      indices.Estimation = (idx - estimationWindowLength):(idx - 1)
-
-      # data points of estimation window
-      return.commodity = commodity[indices.Estimation, ][[attributeOfInterest]]
-      return.portfolio = portfolio[indices.Estimation, ][[attributeOfInterest]]
-
-      switch(regressionType,
-             OLS = {
-               # Ordinary Least Squares
-               M = lm(return.commodity ~ return.portfolio)
-             },
-             {
-               # default case
-               stop(paste(
-                 "Error! Unknown regressionType specified:",
-                 regressionType
-               ))
-             })
-
-
-      # event data points
-      event.return.commodity = commodity[idx, ][[attributeOfInterest]]
-      event.return.portfolio = portfolio[idx, ][[attributeOfInterest]]
-
-      # compute abnormal return
-      nd = data.frame(return.portfolio = event.return.portfolio)
-      abnormal = event.return.commodity - predict(M, newdata = nd)
-
-      collect.abnRet = rbind(
-        collect.abnRet,
-        data.frame(
-          Date = portfolio$Date[idx],
-          abnormalReturn = abnormal,
-          R.squared = summary(M)$r.squared,
-          commodityReturn = event.return.commodity,
-          portfolioReturn = event.return.portfolio
-        )
-      )
-    }
-    # name each row according to it's corresponding index
-    row.names(collect.abnRet) <- indices
-    comment(collect.abnRet) <-
-      paste("abnormalReturns for ",
-            comment(commodity),
-            " (",
-            comment(portfolio),
-            ")",
-            sep = "")
-
-    if (showPlot == TRUE) {
-      title = paste(
-        "Commodity: ",
-        comment(commodity),
-        " - Portfolio: ",
-        comment(portfolio),
-        "\n(",
-        min(commodity$Date),
-        " - ",
-        max(commodity$Date),
-        ")",
-        sep = ""
-      )
-      subtitle = paste('Length of estimation window:', estimationWindowLength)
-
-      # plot whole time series of given commodity
-      plot(
-        commodity$Date,
-        commodity[[attributeOfInterest]],
-        col = 'grey',
-        ylab = paste("Daily", attributeOfInterest),
-        xlab = 'Date',
-        main = title,
-        sub = subtitle
-      )
-
-      legend(
-        'topleft',
-        c("abnormalReturn negative                ", "abnormalReturn positive"),
-        lty = FALSE,
-        col = c('red', 'green'),
-        cex = 1,
-        pch = 4
-      )
-
-      # mark events with positive and negative abnormal returns accordingly.
-      neg = collect.abnRet[collect.abnRet$abnormalReturn <= 0, ]
-      pos = collect.abnRet[collect.abnRet$abnormalReturn > 0, ]
-      points(neg$Date,
-             neg$commodityReturn,
-             col = 'red',
-             pch = 4)
-      points(pos$Date,
-             pos$commodityReturn,
-             col = 'green',
-             pch = 4)
-    }
-
-    return(collect.abnRet)
+#' @export
+computeAbnormalReturn <- function(prices_stock=NULL, prices_market=NULL,
+                                  eventIndex = NULL,
+                                  model = "marketmodel",
+                                  regression = "OLS",
+                                  estimationWindowLength = 20,
+                                  attributeOfInterest = "Close",
+                                  showPlot = FALSE) {
+  if (is.null(prices_stock)) {
+    stop("Argument 'prices_stock' is required.")
   }
+
+  if (model == "marketmodel" && is.null(prices_market)) {
+    stop("Argument 'prices_market' is required for the market model.")
+  }
+
+  ## validate parameters
+
+  if (estimationWindowLength < 5) {
+    stop("An estimation window size of at least 5 time steps is recommend for the regression to produce meaningful results.")
+  }
+
+  # check wether dataset contains attribute Date and attributeOfInterest
+  if (!("Date" %in% names(prices_market))) {
+    stop(paste0("Argument 'prices_market' data' does not contain attribute 'Date'. Available Attributes: ",
+          paste(names(data), collapse = ", ")))
+  }
+
+
+  if (!("Date" %in% names(prices_stock))) {
+    stop(paste0("Error! prices_stock data does not contain attribute 'Date'. Available Attributes: ",
+                paste(names(data), collapse = ", ")))
+  }
+
+  if (!attributeOfInterest %in% names(prices_market)) {
+    stop(paste0("Error! prices_market data does not contain attribute '",
+                attributeOfInterest,
+                "'. Available Attributes: ",
+                paste(names(data), collapse = ", ")))
+  }
+
+  if (!attributeOfInterest %in% names(prices_stock)) {
+    stop(paste0("Error! prices_stock data does not contain attribute '",
+                attributeOfInterest,
+                "'. Available Attributes: ",
+                paste(names(data), collapse = ", "))
+    )
+  }
+
+  # check wether prices_market and prices_stock data sets fit together
+  if (nrow(prices_market) != nrow(prices_stock)) {
+    stop(paste("Error! prices_market and prices_stock data should be of same length:",
+               nrow(prices_market), "vs.", nrow(prices_stock)))
+  }
+
+  if (any(prices_market$Date != prices_stock$Date)) {
+    stop("Error! Dates of prices_market and prices_stock data sets do not match.")
+  }
+
+  # check for which indices the abnormalReturns shall be calculated
+  if (is.null(eventIndex)) {
+    indices <- (estimationWindowLength + 1):nrow(prices_market)
+  } else if (eventIndex > (estimationWindowLength)) {
+    indices <- eventIndex
+  } else {
+    stop("Error! Chosen 'eventIndex' overlaps with 'estimationWindow'. Choose higher eventIndex.")
+  }
+
+  collect.abnRet = c()
+  for (idx in indices) {
+    # indices of data points used for estimating the OLS model
+    indices.Estimation = (idx - estimationWindowLength):(idx - 1)
+
+    # data points of estimation window
+    return.prices_stock = prices_stock[indices.Estimation,][[attributeOfInterest]]
+    return.prices_market = prices_market[indices.Estimation,][[attributeOfInterest]]
+
+    switch(regression,
+           OLS = {
+             # Ordinary Least Squares
+             M = lm(return.prices_stock ~ return.prices_market)
+           },
+           {
+             # default case
+             stop(paste("Error! Unknown regression specified:",
+                        regression))
+           })
+
+
+    # event data points
+    event.return.prices_stock = prices_stock[idx,][[attributeOfInterest]]
+    event.return.prices_market = prices_market[idx,][[attributeOfInterest]]
+
+    # compute abnormal return
+    nd = data.frame(return.prices_market = event.return.prices_market)
+    abnormal = event.return.prices_stock - predict(M, newdata = nd)
+
+    collect.abnRet = rbind(
+      collect.abnRet,
+      data.frame(
+        Date = prices_market$Date[idx],
+        abnormalReturn = abnormal,
+        R.squared = summary(M)$r.squared,
+        prices_stockReturn = event.return.prices_stock,
+        prices_marketReturn = event.return.prices_market
+      )
+    )
+  }
+  # name each row according to it"s corresponding index
+  row.names(collect.abnRet) <- indices
+  comment(collect.abnRet) <-
+    paste("abnormalReturns for ",
+          comment(prices_stock),
+          " (",
+          comment(prices_market),
+          ")",
+          sep = "")
+
+  if (showPlot == TRUE) {
+    plotEventStudy(prices_stock,
+                   prices_market,
+                   attributeOfInterest,
+                   estimationWindowLength,
+                   collect.abnRet)
+  }
+
+  return(collect.abnRet)
+}
+
+#' @importFrom graphics plot legend points
+plotEventStudy <- function(prices_stock, prices_market,
+                           attributeOfInterest,
+                           estimationWindowLength,
+                           collect.abnRet) {
+  title <- paste0("prices_stock: ", comment(prices_stock),
+                  " - prices_market: ", comment(prices_market),
+                  "\n(", min(prices_stock$Date), " - ", max(prices_stock$Date), ")")
+  subtitle <- paste("Length of estimation window:", estimationWindowLength)
+
+  # plot whole time series of given prices_stock
+  plot(prices_stock$Date, prices_stock[[attributeOfInterest]],
+       col = "grey",
+       xlab = "Date", ylab = paste("Daily", attributeOfInterest),
+       main = title,
+       sub = subtitle)
+
+  legend("topleft", c("abnormalReturn negative                ",
+                      "abnormalReturn positive"),
+         lty = FALSE,
+         col = c("red", "green"),
+         cex = 1, pch = 4)
+
+  # mark events with positive and negative abnormal returns accordingly.
+  neg = collect.abnRet[collect.abnRet$abnormalReturn <= 0, ]
+  pos = collect.abnRet[collect.abnRet$abnormalReturn > 0, ]
+
+  points(neg$Date,
+         neg$prices_stockReturn,
+         col = "red",
+         pch = 4)
+  points(pos$Date,
+         pos$prices_stockReturn,
+         col = "green",
+         pch = 4)
+}
 
 # # load data
 # d.DAX <- read.csv("data/DAX_2015.csv", stringsAsFactors=FALSE)
 # d.DAX$Date <- as.POSIXct(d.DAX$Date)
 # d.DAX <- d.DAX[order(d.DAX$Date),]
-# comment(d.DAX) <- 'DAX'
+# comment(d.DAX) <- "DAX"
 #
 # d.Adidas <- read.csv("data/DAX_2015_Adidas.csv", stringsAsFactors=FALSE)
 # d.Adidas <- d.Adidas[d.Adidas$Volume != 0, ]
 # d.Adidas$Date <- as.POSIXct(d.Adidas$Date)
 # d.Adidas <- d.Adidas[order(d.Adidas$Date),]
-# comment(d.Adidas) <- 'Adidas'
+# comment(d.Adidas) <- "Adidas"
 #
 # d.VW <- read.csv("data/DAX_2015_VW.csv", stringsAsFactors=FALSE)
 # d.VW <- d.VW[d.VW$Volume != 0, ]
 # d.VW$Date <- as.POSIXct(d.VW$Date)
 # d.VW <- d.VW[order(d.VW$Date),]
-# comment(d.VW) <- 'VW'
+# comment(d.VW) <- "VW"
 #
 # # compute abnormal Returns
-# abnormal = computeAbnormalReturn(portfolio=d.DAX, commodity=d.VW, regressionType='OLS',
-#                                  eventIndex=NULL, estimationWindowLength=20, attributeOfInterest='Close',
+# abnormal = computeAbnormalReturn(prices_market=d.DAX, prices_stock=d.VW, regression="OLS",
+#                                  eventIndex=NULL, estimationWindowLength=20, attributeOfInterest="Close",
 #                                  showPlot=TRUE)
 # print(mean(abnormal$R.squared))
 # print(var(abnormal$R.squared))
--- tests/testthat/test.abnormalReturn.R
+++ tests/testthat/test.abnormalReturn.R
@@ -2,89 +2,53 @@ library(testthat)
 context("Abnormal returns")
 
 test_that("function does not throw any errors or warnings if called with default settings", {
-  expect_silent(computeAbnormalReturn(portfolio=d.DAX, commodity=d.VW))
-  expect_silent(computeAbnormalReturn(portfolio=d.DAX, commodity=d.VW, showPlot = TRUE))
+  expect_silent(computeAbnormalReturn(prices_market = d.DAX, prices_stock = d.VW))
+  expect_silent(computeAbnormalReturn(prices_market = d.DAX, prices_stock = d.VW, showPlot = TRUE))
 })
 
 test_that("returned data frame has correct dimensions (eventIndex==NULL)", {
   windowLength = 20
-  x <- computeAbnormalReturn(portfolio=d.DAX, commodity=d.VW, eventIndex=NULL,
+  x <- computeAbnormalReturn(prices_market = d.DAX, prices_stock = d.VW, eventIndex=NULL,
                              estimationWindowLength=windowLength)
   expect_equal(nrow(x), nrow(d.DAX) - windowLength)
   expect_equal(names(x), c("Date", "abnormalReturn", "R.squared", "commodityReturn", "portfolioReturn"))
 })
 
 test_that("returned data frame has correct dimensions (eventIndex!=NULL)", {
-  x <- computeAbnormalReturn(portfolio=d.DAX, commodity=d.VW, eventIndex=50)
+  x <- computeAbnormalReturn(prices_market = d.DAX, prices_stock = d.VW, eventIndex=50)
   expect_equal(nrow(x), 1)
   expect_equal(names(x), c("Date", "abnormalReturn", "R.squared", "commodityReturn", "portfolioReturn"))
 })
 
 test_that("portfolio and commodity are checked for conformity", {
-  expect_that(
-    computeAbnormalReturn(portfolio=d.DAX[1:252,], commodity=d.VW),
-    throws_error("Error! Portfolio and commodity data should be of same length: 252 vs. 253")
-  )
+  expect_error(computeAbnormalReturn(prices_market = d.DAX[1:252,], prices_stock = d.VW))
 
-  expect_that(
-    computeAbnormalReturn(portfolio=d.DAX, commodity=d.VW[1:10,]),
-    throws_error("Error! Portfolio and commodity data should be of same length: 253 vs. 10")
-  )
+  expect_error(computeAbnormalReturn(prices_market = d.DAX, prices_stock = d.VW[1:10,]))
 
-  expect_that(
-    computeAbnormalReturn(portfolio=d.DAX[,2:7], commodity=d.VW),
-    throws_error("Error! Portfolio data does not contain attribute 'Date'")
-  )
+  expect_error(computeAbnormalReturn(prices_market = d.DAX[,2:7], prices_stock = d.VW))
 
-  expect_that(
-    computeAbnormalReturn(portfolio=d.DAX, commodity=d.VW[,2:7]),
-    throws_error("Error! Commodity data does not contain attribute 'Date'")
-  )
+  expect_error(computeAbnormalReturn(prices_market = d.DAX, prices_stock = d.VW[,2:7]))
 
-  expect_that(
-    computeAbnormalReturn(portfolio=d.DAX, commodity=d.VW, attributeOfInterest = "xyz"),
-    throws_error("Error! Portfolio data does not contain attribute 'xyz'")
-  )
+  expect_error(computeAbnormalReturn(prices_market = d.DAX, prices_stock = d.VW, attributeOfInterest = "xyz"))
 
-  expect_that(
-    computeAbnormalReturn(portfolio=d.DAX, commodity=d.VW[,1:3]),
-    throws_error("Error! Commodity data does not contain attribute 'Close'")
-  )
+  expect_error(computeAbnormalReturn(prices_market = d.DAX, prices_stock = d.VW[,1:3]))
 
-  expect_that(
-    computeAbnormalReturn(portfolio=d.DAX[,1:3], commodity=d.VW),
-    throws_error("Error! Portfolio data does not contain attribute 'Close'")
-  )
+  expect_error(computeAbnormalReturn(prices_market = d.DAX[,1:3], prices_stock = d.VW))
 
   d.DAX_mod = d.DAX
   d.DAX_mod$Date[2] = "2015-01-04 CET"
-  expect_that(
-    computeAbnormalReturn(portfolio=d.DAX_mod, commodity=d.VW),
-    throws_error("Error! Dates of portfolio and commodity data sets do not match.")
-  )
+  expect_error(computeAbnormalReturn(prices_market = d.DAX_mod, prices_stock = d.VW))
   remove(d.DAX_mod)
 })
 
 test_that("parameter estimationWindowLength is set correct", {
-  expect_that(
-    computeAbnormalReturn(portfolio=d.DAX, commodity=d.VW, estimationWindowLength=2),
-    throws_error("Error! A minimum estimation window size of 3")
-  )
+  expect_error(computeAbnormalReturn(prices_market = d.DAX, prices_stock = d.VW, estimationWindowLength = 2),)
 
-  expect_that(
-    computeAbnormalReturn(portfolio=d.DAX, commodity=d.VW, estimationWindowLength=-20),
-    throws_error("Error! A minimum estimation window size of 3")
-  )
+  expect_error(computeAbnormalReturn(prices_market = d.DAX, prices_stock = d.VW, estimationWindowLength = -20))
 
-  expect_that(
-    computeAbnormalReturn(portfolio=d.DAX, commodity=d.VW, eventIndex = 20, estimationWindowLength = 20),
-    throws_error("Error! Chosen eventIndex overlaps with estimationWindow.")
-  )
+  expect_error(computeAbnormalReturn(prices_market = d.DAX, prices_stock = d.VW, eventIndex = 20, estimationWindowLength = 20))
 })
 
 test_that("error thrown, when bad regressionType specified", {
-  expect_that(
-    computeAbnormalReturn(portfolio=d.DAX, commodity=d.VW, regressionType = 'xyz'),
-    throws_error("Error! Unknown regressionType specified: xyz")
-  )
+  expect_error(computeAbnormalReturn(prices_market = d.DAX, prices_stock = d.VW, regressionType = 'xyz'))
 })
